6e6b026559dc6e79598b534a2275da459a82c058
Sanjeev Malagi <sanjeevmalagi@gmail.com>
2025-05-06 20:15:21 +0530

    Make the html editable using contentEditable

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import './style.css'
 
+const editSaveButton = document.getElementById('edit-content-button');
+
+editSaveButton.addEventListener('click', () => {
+  const isEditing = document.body.contentEditable === 'true';
+  document.body.contentEditable = isEditing ? 'false' : 'true';
+  editSaveButton.textContent = isEditing ? 'Edit' : 'Save';
+});
+
+const printButton = document.getElementById('print-button');
+
+printButton.addEventListener('click', () => {
+  window.print();
+});
