73ae9743c80a59a113216fbde25451567352e729
Sanjeev Malagi <sanjeevmalagi@gmail.com>
2025-05-07 08:28:48 +0530

    use localstoreage to save cv content and add restore option

--- src/main.js
+++ src/main.js
@@ -1,14 +1,44 @@
 import './style.css'
 
-const editSaveButton = document.getElementById('edit-content-button');
+const LOCALSTORAGE_NAME = "cv_content";
+const CV_CONTENT_ID = "cv-content";
 
-editSaveButton.addEventListener('click', () => {
-  const isEditing = document.body.contentEditable === 'true';
-  document.body.contentEditable = isEditing ? 'false' : 'true';
-  editSaveButton.textContent = isEditing ? 'Edit' : 'Save';
+const editSaveButton = document.getElementById('edit-content-button');
+const printButton = document.getElementById('print-button');
+const resetButton = document.getElementById('reset');
+
+const saveToLocalStorage = () => {
+  const bodyHTML = document.getElementById(CV_CONTENT_ID).innerHTML;
+  localStorage.setItem(LOCALSTORAGE_NAME, bodyHTML);
+}
+
+const restoreFromLocalStorage = () => {
+  const savedHTML = localStorage.getItem(LOCALSTORAGE_NAME);
+  if (savedHTML !== null) {
+    document.getElementById(CV_CONTENT_ID).innerHTML = savedHTML;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  restoreFromLocalStorage();
 });
 
-const printButton = document.getElementById('print-button');
+resetButton.addEventListener('click', () => {
+  localStorage.removeItem(LOCALSTORAGE_NAME);
+  location.reload();
+});
+
+editSaveButton.addEventListener('click', () => {
+  const cvContentDiv = document.getElementById(CV_CONTENT_ID);
+
+  const isEditing = cvContentDiv.contentEditable === 'true';
+  cvContentDiv.contentEditable = isEditing ? 'false' : 'true';
+  editSaveButton.textContent = isEditing ? 'Edit' : 'Save';
+
+  if (isEditing) {
+    saveToLocalStorage();
+  }
+});
 
 printButton.addEventListener('click', () => {
   window.print();
